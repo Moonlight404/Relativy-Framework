42b4777b288423ae5cccf550cc9cd19343e42163
Moonlight404 <59587363+Moonlight404@users.noreply.github.com>
2020-02-11 20:47:32 -0300

    Initial

---
+++ app/routes_engine.php
@@ -0,0 +1,25 @@
+<?php
+function route($link, $file){
+    $ht = fopen("./.htaccess","a");
+    fwrite($ht, "RewriteRule ^$link /views/$file.php [L]\n");
+}
+
+function clear(){
+    $ht = fopen("./.htaccess","w");
+    //open file to write
+    // clear content to 0 bits
+    ftruncate($ht, 0);
+    //close file
+}
+
+function rewrite(){
+    $ht = fopen("./.htaccess","a");
+    fwrite($ht, "RewriteEngine on\n");
+}
+
+function start(){
+    clear();
+    rewrite();
+}
+
+start();
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+//Routes engine
+require('app/routes_engine.php');
+
+//Autoload
+include('bootstrap/autoload.php');
+
+require('routes/web.php');
+
+require('app/template.php');
+
