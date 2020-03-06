ebbd9e93162ef66d4b8347c09d33f27c11acc879
kaway404 <kaway404@kaway404.kaway404>
2020-03-06 15:51:06 -0300

    fIX

---
+++ app/server.php
@@ -0,0 +1,10 @@
+<?php
+function serverInit(){
+    $str = file_get_contents('env.json');
+    $json = json_decode($str, true);
+    $port = $json['port'];
+    $exec = "cd public && php -S localhost:$port && cls && clear";
+    echo "Aguarde um momento";
+    echo "\nServer iniciado com sucesso\n Acesse usando http://127.0.0.1:$port\n Logs: ";
+    $server = shell_exec($exec);
+}
--- server.php
+++ server.php
@@ -5,10 +5,7 @@
  * @package  Relavify
  * @author   Alexandre Silva
  */
- $str = file_get_contents('env.json');
- $json = json_decode($str, true);
- $port = $json['port'];
- $exec = "cd public && php -S localhost:$port && cls && clear";
- echo "Aguarde um momento";
- echo "\nServer iniciado com sucesso\n Acesse usando http://127.0.0.1:$port\n Logs: ";
- $server = shell_exec($exec);
+require ('app/autoload.php');
+
+//Inicia server
+serverInit();
